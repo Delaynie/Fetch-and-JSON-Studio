c0698a879b27aa391e2cef451a0f7e5fbf0edae9
Delaynie <apple@Delaynies-MacBook-Pro.local>
2020-12-08 16:27:09 -0600

    added javascript code

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// TODO: add code here
+// TODO: first, add window load
+window.addEventListener("load", function(){
+    // TODO: next make a fetch
+        let json = [];
+        fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+            response.json().then(function(json) {
+                const destination = document.getElementById("destination");
+                    let index = 0;
+                    destination.addEventListener("click", function(){
+                    destination.innerHTML = `
+                        <div>
+                            <h3>Planet ${json[index].name}</h3>
+                            <img src=${json[index].image} height=250></img>
+                        </div>
+                    `;
+            index = (index + 1) % json.length;
+        });
+    });
+});
+});
