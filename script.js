bb3a3d2243044be8bd634ac6b24c0f87511cd632
Delaynie <apple@Delaynies-MacBook-Pro.local>
2020-12-08 16:42:32 -0600

    updated javascript

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 window.addEventListener("load", function(){
     // TODO: next make a fetch
         let json = [];
-        fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+        fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
             response.json().then(function(json) {
-                const destination = document.getElementById("destination");
+                const container = document.getElementById("container");
                     let index = 0;
-                    destination.addEventListener("click", function(){
-                    destination.innerHTML = `
+                    container.addEventListener("click", function(){
+                    container.innerHTML = `
                         <div>
                             <h3>Planet ${json[index].name}</h3>
                             <img src=${json[index].image} height=250></img>
